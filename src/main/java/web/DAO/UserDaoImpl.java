498bd2eea2e5b6c4d3bc4115824f6af7423225c4
Arun Verma <arun.ver09@gmail.com>
2016-05-04 15:48:37 +0530

    Updated spring security error message code.

--- src/main/java/web/DAO/UserDaoImpl.java
+++ src/main/java/web/DAO/UserDaoImpl.java
@@ -2,6 +2,7 @@ package web.DAO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import web.MODEL.UserInfo;
 
@@ -29,13 +30,22 @@ public class UserDaoImpl implements UserDao {
 		
 		DBObject userData = collection.findOne(user);
 		
+		if(userData!=null){
+		
 		UserInfo userAuth= new UserInfo();
 		userAuth.setUsername((String)userData.get("username"));
 		userAuth.setPassword((String)userData.get("password"));
 		userAuth.setRole((String)userData.get("role"));
+		return userAuth;
+		}
+		else
+		{
+			throw new UsernameNotFoundException("User not found");
+			
+		}
 		
 		//System.out.println(userAuth.getRole());
-		return userAuth;
+		
 	}
 	
 
